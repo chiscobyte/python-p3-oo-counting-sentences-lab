cec75c109d4b20f68ff60af429dad88c5326a462
chiscobyte <chiscoenock@gmail.com>
2025-05-18 18:33:16 +0300

    initiate

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,34 @@
 #!/usr/bin/env python3
 
+import re
+
 class MyString:
-  pass
+    def __init__(self, value=''):
+        self._value = ''
+        self.value = value  # Uses the setter to validate
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        if isinstance(new_value, str):
+            self._value = new_value
+        else:
+            print("The value must be a string.")
+
+    def is_sentence(self):
+        return self._value.endswith('.')
+
+    def is_question(self):
+        return self._value.endswith('?')
+
+    def is_exclamation(self):
+        return self._value.endswith('!')
+
+    def count_sentences(self):
+        # Replace all sentence-ending punctuation with periods for uniform splitting
+        standardized = re.sub(r'[.!?]', '.', self._value)
+        parts = [sentence.strip() for sentence in standardized.split('.') if sentence.strip()]
+        return len(parts)
